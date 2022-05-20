d525c6b3b5ec695449b1b73e1bf5fffb1715efaa
David Roos <inf102396@fh-wedel.de>
2022-05-20 22:22:01 +0200

    utilities

--- code/inc/utils.h
+++ code/inc/utils.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "includes.h"
 
-uint8_t util_strlen(int8_t * s);
+typedef struct {
+    uint8_t bFlagDebugging :1;
+    uint8_t bFlagUnused :7;
+} T_UTIL_CFG;
 
+uint8_t util_strlen(int8_t * s);
 void util_debug(char * s, ...);
 void util_swap(uint16_t * p);
--- code/src/utils.c
+++ code/src/utils.c
@@ -1,18 +1,26 @@
 #include "includes.h"
 #include <stdarg.h>
 
-#ifdef DEBUGGING_ON
-void util_debug(char * f, ...) {
+static T_UTIL_CFG * cfg;
+
+void _util_debug_on(char * f, ...) {
     fprintf(stdout, "DEBUGGING...\n");
     va_list a;
     va_start(a,f);
     vfprintf(stdout,f,a);
     va_end(a);
 }
-#else
-void util_debug(char * s, ...) {
+void _util_debug_off(char * s, ...) {
 }
-#endif
+
+void (*_util_debug[])(char * s, ...) = {
+    _util_debug_off, _util_debug_on
+};
+
+void util_debug(char * s, ...) {
+    va_list a;
+    _util_debug[cfg->bFlagDebugging](s,a);
+}
 
 uint8_t util_strlen(int8_t * s) {
     uint8_t l = 0;
@@ -22,4 +30,8 @@ uint8_t util_strlen(int8_t * s) {
 
 void util_swap(uint16_t * p) {
     *p = ((*p & 0x00ff) << 8) | ((*p & 0xff00) >> 8);
+}
+
+void util_init(T_UTIL_CFG * ptConfig) {
+    cfg = ptConfig;
 }
